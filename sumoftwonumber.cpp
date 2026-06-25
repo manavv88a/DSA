591ae211733846a4b38b4455c05055e15293d864
Manav verma <manavv88a@gmail.com>
2026-06-25 07:22:00 +0530

    .

--- sumoftwonumber.cpp
+++ sumoftwonumber.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
-        unordered_map<int, int> mp;
+        unordered_map <int, int> mp;
 
         for(int i = 0; i < nums.size(); i++) {
             int complement = target - nums[i];
