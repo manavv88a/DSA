461e9149f4a3c1672c95c631cd5f9f173f971edb
Manav verma <manavv88a@gmail.com>
2026-06-25 07:37:48 +0530

    .

---
+++ radixsort.cpp
@@ -0,0 +1,55 @@
+#include <iostream>
+using namespace std;
+
+
+int getMax(int arr[], int n) {
+    int mx = arr[0];
+    for (int i = 1; i < n; i++)
+        if (arr[i] > mx)
+            mx = arr[i];
+    return mx;
+}
+void countSort(int arr[], int n, int exp) {
+    int output[n];
+    int count[10] = {0};
+
+    
+    for (int i = 0; i < n; i++)
+        count[(arr[i] / exp) % 10]++;
+
+    
+    for (int i = 1; i < 10; i++)
+        count[i] += count[i - 1];
+
+    
+    for (int i = n - 1; i >= 0; i--) {
+        output[count[(arr[i] / exp) % 10] - 1] = arr[i];
+        count[(arr[i] / exp) % 10]--;
+    }
+
+    // Copy back
+    for (int i = 0; i < n; i++)
+        arr[i] = output[i];
+}
+
+// Radix Sort function
+void radixSort(int arr[], int n) {
+    int m = getMax(arr, n);
+
+    // Sort for every digit
+    for (int exp = 1; m / exp > 0; exp *= 10)
+        countSort(arr, n, exp);
+}
+
+int main() {
+    int arr[] = {170, 45, 75, 90, 802, 24, 2, 66};
+    int n = 8;
+
+    radixSort(arr, n);
+
+    cout << "Sorted array: ";
+    for (int i = 0; i < n; i++)
+        cout << arr[i] << " ";
+
+    return 0;
+}
